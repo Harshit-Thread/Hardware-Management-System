d56b91c28e65837c7e79e017fea18b8199687673
Harshit <94279595+Harshit-Thread@users.noreply.github.com>
2023-03-09 21:46:03 +0530

    Add files via upload

---
+++ Hardware Management System/admin/includes/footer.php
@@ -0,0 +1,10 @@
+   <section class="footer-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                   &copy; <?php echo date('Y');?> Online Hardware Management System |<a href="https://fcsglobal.in/" target="_blank"  > Designed by : Trio </a> 
+                </div>
+
+            </div>
+        </div>
+    </section>
